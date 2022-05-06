ad9f1d877341cd471af3792263e877bcc0418df0
lichengkun <lichengkun@hinterstellar.com>
2022-05-06 17:23:09 +0800

    fix: 修改loadCocos.js

--- game2d/games-build-templates/common/loadCocos.js
+++ game2d/games-build-templates/common/loadCocos.js
@@ -21,5 +21,27 @@
     }
 
     NDBLoadRemoteCocos();
+
+    function loadScript(moduleName, cb) {
+      function scriptLoaded() {
+        document.body.removeChild(domScript);
+        domScript.removeEventListener('load', scriptLoaded, false);
+        cb && cb();
+      };
+      var domScript = document.createElement('script');
+      domScript.async = true;
+      domScript.src = moduleName;
+      domScript.addEventListener('load', scriptLoaded, false);
+      document.body.appendChild(domScript);
+    }
+
+    loadScript(debug ? 'cocos2d-js.js' : 'https://a.fslk.co/games/cocos2d-min-js-2.4.8-no3d.js', function () {
+      if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
+        loadScript(debug ? 'physics.js' : 'physics-min.js', window.boot);
+      }
+      else {
+        window.boot();
+      }
+    });
     
 })();
--- game2d/games-build-templates/common/ndb.js
+++ game2d/games-build-templates/common/ndb.js
@@ -287,49 +287,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
           console.log('===========onDownloadFinish=============');
 
           //获取页面参数值;
-          function getUrlParameterValue(name) {
-            let href = window.location.href;
-            var query = href.substring(1);
-            var vars = query.split("?");
-            if (!vars[1]) {
-              return '';
-            }
-            var argment = vars[1].split("&");;
-            for (var i = 0; i < argment.length; i++) {
-              var pair = argment[i].split("=");
-              if (pair[0] == name) { return pair[1]; }
-            }
-            return '';
-          }
-          let u = navigator.userAgent;
-          let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;   //判断是否是 android终端
-          let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);     //判断是否是 iOS终端
-          var vCode = Number(getUrlParameterValue('vcode'));
-          var loadJs = "cocos2d-min-js-" + "v2.4.8.js";
+          // function getUrlParameterValue(name) {
+          //   let href = window.location.href;
+          //   var query = href.substring(1);
+          //   var vars = query.split("?");
+          //   if (!vars[1]) {
+          //     return '';
+          //   }
+          //   var argment = vars[1].split("&");;
+          //   for (var i = 0; i < argment.length; i++) {
+          //     var pair = argment[i].split("=");
+          //     if (pair[0] == name) { return pair[1]; }
+          //   }
+          //   return '';
+          // }
+          // let u = navigator.userAgent;
+          // let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;   //判断是否是 android终端
+          // let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);     //判断是否是 iOS终端
+          // var vCode = Number(getUrlParameterValue('vcode'));
+          // var loadJs = "cocos2d-min-js-" + "v2.4.8.js";
 
-          var isProd = window.location.href.indexOf('prod') >= 0 ? true : false;
+          // var isProd = window.location.href.indexOf('prod') >= 0 ? true : false;
 
-          function loadScript(moduleName, cb) {
-            function scriptLoaded() {
-              document.body.removeChild(domScript);
-              domScript.removeEventListener('load', scriptLoaded, false);
-              cb && cb();
-            };
-            var domScript = document.createElement('script');
-            domScript.async = true;
-            domScript.src = moduleName;
-            domScript.addEventListener('load', scriptLoaded, false);
-            document.body.appendChild(domScript);
-          }
-
-          loadScript(debug ? 'cocos2d-js.js' : 'https://a.fslk.co/games/cocos2d-min-js-2.4.8-no3d.js', function () {
-            if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-              loadScript(debug ? 'physics.js' : 'physics-min.js', window.boot);
-            }
-            else {
-              window.boot();
-            }
-          });
+          
           
         };
         //当前zip下载进度;
